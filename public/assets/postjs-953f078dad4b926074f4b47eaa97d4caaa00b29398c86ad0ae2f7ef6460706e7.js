ef546c335602042ffd0aa3b468443e0a6f326322
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-07-06 18:51:53 -0400

    trivial

---
+++ public/assets/postjs-953f078dad4b926074f4b47eaa97d4caaa00b29398c86ad0ae2f7ef6460706e7.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var preview = $(".cover_image_preview img");
+
+    $("#cover_image_uploader").change(function(event){
+       var input = $(event.currentTarget);
+       var file = input[0].files[0];
+       var reader = new FileReader();
+       var ht = preview.attr("height");
+       var wd = preview.attr("width");
+       reader.onload = function(e){
+           image_base64 = e.target.result;
+           preview.attr("src", image_base64);
+           preview.attr("width", $(".cover_image_preview").width());
+           preview.attr("height", ($(".cover_image_preview").width() / wd) * ht);
+       };
+       reader.readAsDataURL(file);
+    });
+});
+
+// problem w/ img dimensions for horz longer images, width not capped at 300
+// attach img, then no file chosen, img still displays in preview
+// how to do a remove attachment button
+;
