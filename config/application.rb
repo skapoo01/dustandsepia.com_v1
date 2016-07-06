eb1cba4b7cac1942e19f6b853cad3769d509d77c
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-07-06 15:40:04 -0400

    trivial

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module DustandsepiaComV1
     config.assets.enabled = true
     #config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.paths << Rails.root.join('/app/assets/fonts')
+
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
