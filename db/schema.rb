eb9aea863e0628843a4d73a4fac3b5848741ac4f
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-07-08 12:46:56 -0400

    work in progress on users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702034703) do
+ActiveRecord::Schema.define(version: 20160708164412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,5 +39,14 @@ ActiveRecord::Schema.define(version: 20160702034703) do
 
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "name"
+    t.boolean  "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "active"
+  end
 
 end
