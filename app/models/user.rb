e59639f342d9fe46b59e8a22dc131c6fa95f1009
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-07-09 10:21:50 -0400

    user login w/ username and email working, but email is case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable
+ 
+  attr_accessor :login
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+
+validate :validate_username
+
+
 
 
 #  def email_required?
@@ -22,6 +37,30 @@ class User < ActiveRecord::Base
     unless @user == current_user
       redirect_to :back, :alert => "Access denied."
     end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_hash).first
+      end
+  end
+
+
+def validate_username
+  if User.where(email: username).exists?
+    errors.add(:username, :invalid)
+  end
+end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
 
